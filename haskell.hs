eb01a39baf7272eabd507b8715f875ad42196b00
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-11-22 01:04:43 +0100

    feat: Adding haskell learning course

---
+++ haskell.hs
@@ -0,0 +1,7 @@
+-- not is a function who will give the !== of an element 
+getOppositBool :: Bool -> Bool
+getOppositBool e = not e
+
+-- Another way to write it : 
+getOppositBool' :: Bool -> Bool
+getOppositBool' = not
