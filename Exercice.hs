f01261863b64c98d8cacc249e21cbda3156c8ad6
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-11-25 07:06:37 +0100

    Get year on planet exercice

--- Exercice.hs
+++ Exercice.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 isLeapYear :: Integer -> Bool
 isLeapYear year 
     | isDivisibleBy 400 = True
@@ -5,3 +6,31 @@ isLeapYear year
     | isDivisibleBy 4 = True
     | otherwise = False
     where isDivisibleBy x = mod year x == 0
+
+-- 1year === 31557600
+-- ?? === 1_000_000_000
+
+data Planet = Mercury | Venus | Earth | Mars | Jupiter | Saturn | Uranus | Neptune 
+
+earthYearsTo :: Planet -> Float
+earthYearsTo planet = case planet of
+    Mercury -> 0.2408467
+    Venus -> 0.61519726
+    Earth -> 1.0
+    Mars -> 1.8808158
+    Jupiter -> 11.862615
+    Saturn -> 29.447498
+    Uranus -> 84.016846
+    Neptune -> 164.79132
+
+multiply :: Float -> Float
+multiply x = x * 31557600
+
+getResult :: Float -> Float -> Float
+getResult x y = x / multiply y
+
+ageOn :: Planet -> Float -> Float
+ageOn planet seconds = getResult seconds (earthYearsTo planet )
+
+-- correction
+
